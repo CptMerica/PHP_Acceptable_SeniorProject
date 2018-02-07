112986de80b1bb0938d37ca83c807455c3f2b257
Stephen Terhorst <ter2198@calu.edu>
2018-02-07 17:48:12 -0500

    PHP project

--- PACareerLinkRegistration/PACareerLinkRegistration/index.php
+++ PACareerLinkRegistration/PACareerLinkRegistration/index.php
@@ -1,5 +1,21 @@
-<?php
+<html>
+    <head>
+        <title>Login Page</title>
 
-echo "Hello World!";
+    </head>
+        <body>
+            <div>
+                <p>Usernme:    <input id="Username" type="text"  />  </p>
+                <br />
+                <p>Password: <input id="Password1" type="password" /> </p>
+            </div>
+            <?php
 
-?>
+
+                
+            
+
+            ?>
+        </body>
+</html>
+
---
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Script1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title></title>
+</head>
+    <body>
+        <p>Please Login to gain access.</p>
+        <p>Username:<input id="username" type="text" /></p>
+        <br />
+        <p>Password:<input id="password" type="text" /></p>
+        <?php
+
+
+        ?>
+    </body>
+</html>
