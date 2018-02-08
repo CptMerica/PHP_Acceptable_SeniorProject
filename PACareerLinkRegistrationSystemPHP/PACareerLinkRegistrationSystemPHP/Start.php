5a53ca70c4d08befdd8e4e776f0d2457f7e5f893
Stephen Terhorst <ter2198@calu.edu>
2018-02-08 17:10:19 -0500

    Started registration form

--- PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Start.php
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Start.php
@@ -1,15 +1,24 @@
 <html>
 <head>
     <title></title>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
 </head>
     <body>
         <p>Please Login to gain access.</p>
+        
+     <form method="post">
+        <h1><img src="pacareerlink.jpg" style="height: 93px; width:459px;" /></h1>
         <p>Username:<input id="username" type="text" /></p>
         <br />
-        <p>Password:<input id="password" type="text" /></p>
+        <p>Password:<input id="password" type="password" /></p>
+        <input id="LoginButton" type="submit" value="Login" />
+        
+     </form>   
+        
+        
         <?php
-
-
+            
+   
         ?>
     </body>
 </html>
