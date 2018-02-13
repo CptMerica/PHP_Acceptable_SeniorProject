3475a8317e494496efb58f59ab9ffea26f37b714
Stephen Terhorst <ter2198@calu.edu>
2018-02-12 21:05:23 -0500

    Created Variables for some inputs

--- PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/RegistrationForm.php
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/RegistrationForm.php
@@ -1,39 +1,125 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
-        <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
-    </head>
-        <body>
-            <h1><img src="pacareerlink.jpg" style="height: 93px; width:459px;" /></h1>
-            
-            <h2>Registration Form</h2>
-            <section>
-            <p>First Name: <input id="fname" type="text" /></p>
-            <p>Last Name: <input id="lname" type="text" /></p>
-            <p>Last four digits of Social Security #:<input id="Text1" type="text" /></p>
-            </section>
-                <br />
-            <p>Education:</p>
-            <section>
-            <p>High School Diploma/GED<input id="GEDCheckbox" type="checkbox" /></p> 
-            <p>Certification <input id="CertCheckbox" type="checkbox" /></p>
-            <p>Associate's Degree <input id="ADCheckbox" type="checkbox" /></p>
-            <p>Bachelor's Degree<input id="BDegreeCheckbox" type="checkbox" /></p>
-            <p>Master's Degree <input id="MDegreeCheckbox" type="checkbox" /></p>
-            <p>Doctorate <input id="phdCheckbox" type="checkbox" /></p>
-            </section>
-            <p>County of Residence:</p>
-            <section>
-                <p>Washington<input id="washcountyCheckbox" type="checkbox" /></p>
-                <p>Fayette<input id="FayetteCheckbox" type="checkbox" /></p>
-                <p>Greene<input id="GreeneCheckbox" type="checkbox" /></p>
-                <p>Westmoreland<input id="Westmorelandcheckbox" type="checkbox" /></p>
-                <p>Other<input id="OtherCCheckbox" type="checkbox" /></p>
-            </section>
-            <?php
-            
-            ?>
+<head>
+    <title> Welcome to Registration</title>
+    <meta charset="utf-8" />
+    <link href="SystemStyleSheet.css" rel="stylesheet" />
+</head>
+<body>
+    <h1> <img src="pacareerlink.jpg" style="height: 93px; width:459px;" </h1>
+    <h2> Registration Form</h2>
+    <p> Please Complete the form below to register. </p>
+    <form method="post">
+        <p>First Name:<input name="fname" type="text" /> </p>
+        <p>Last Name: <input name="lname" type="text" /></p>
+        <p>Last 4 of Social Security Number: <input name="ssnumber" type="text" /> </p>
+        <br />
+        <p>Education: </p>
+        <select id="EducationSelect">
+            <option> </option>
+            <option> None < </option>
+            <option> GED </option>
+            <option> High School Diploma </option>
+            <option> Associate's Degree </option>
+            <option> Bachelor's Degree </option>
+            <option> Master's Degree</option>
+            <option> Doctorate's </option>
+        </select>
 
-        </body>
+        <p>County of Residence:</p>
+        <select id="CountySelect">
+            <option> </option>
+            <option> Washington </option>
+            <option> Fayette </option>
+            <option> Westmoreland </option>
+            <option> Allegheny </option>
+            <option> Indiana </option>
+            <option> Beaver </option>
+            <option> Butler </option>
+            <option> Greene </option>
+            <option> Other </option>
+        </select>
+
+        <p>Employment Status:</p>
+        <select id="EmploymentSelect">
+            <option> </option>
+            <option> Unemployed </option>
+            <option> Employed </option>
+            <option> Dislocated</option>
+        </select>
+        <p>Purpose of Visit:</p>
+        <ul>
+            <li>EARN<input id="Earn" type="checkbox" /></li>
+            <li>Apprenticeship Information <input id="ApprenticeInfo" type="checkbox" /></li>
+            <li>Prep Class<input id="PrepClass" type="checkbox" /></li>
+            <li>Education and Training<input id="Education_Training" type="checkbox" /></li>
+            <li>Scheduled Appointment<input id="SchedApt" type="checkbox" /></li>
+            <li>Employer Recruitment<input id="EmpRecruit" type="checkbox" /></li>
+            <li>Employment Testing<input id="EmpTrain" type="checkbox" /></li>
+            <li>GED/Adult Remediation<input id="GED_Adult" type="checkbox" /></li>
+            <li>UC Hotline<input id="UCHotline" type="checkbox" /></li>
+            <li>Job Order Listing<input id="JobOrder" type="checkbox" /></li>
+            <li>Workshop<input id="Workshop" type="checkbox" /></li>
+            <li>Job Search/Application<input id="JobSearch" type="checkbox" /></li>
+            <li>JobGateway Enrollment<input id="JobGatewayEnroll" type="checkbox" /></li>
+            <li>OVR<input id="OVR" type="checkbox" /></li>
+        </ul>
+
+        <br />
+
+        <p>Are you a veteran? </p>
+        <ul>
+            <li>Yes <input id="VetYes" type="checkbox" /></li>
+            <li>No <input id="VetNo" type="checkbox" /></li>
+        </ul>
+
+        <p>Are you over 55?</p>
+        <ul>
+            <li>Yes <input id="OverYes" type="checkbox" /></li>
+            <li>No <input id="OverNo" type="checkbox" /></li>
+        </ul>
+        <br />
+
+        <p>Are you between the ages of 14 and 24?</p>
+        <ul>
+            <li>Yes <input id="YouthYes" type="checkbox" /></li>
+            <li>o <input id="YouthNo" type="checkbox" /></li>
+        </ul>
+        <br />
+
+        <p>Are you receiving Government Assistance?</p>
+        <ul>
+            <li>Yes <input id="GovAssistYes" type="checkbox" /></li>
+            <li> No <input id="GovAssistNo" type="checkbox" /></li>
+        </ul>
+        <br />
+        <input name="submit" type="submit" value="Submit" />
+
+    </form>
+
+    <?php
+     
+        if(isset ($_POST['fname']) )
+        {
+            $fname = $_POST['fname'];
+            $lname=$_POST['lname'];
+            $ssnumber=$_POST['ssnumber'];
+            
+            echo $fname;
+            echo $lname;
+            echo $ssnumber;
+            
+        }
+            
+       
+        
+    
+  
+
+    ?>
+
+
+</body>
 </html>
+
 
--- PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Start.php
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Start.php
@@ -16,7 +16,7 @@
     </h1>
     <h2>Registration System</h2>
     <p>Please log in to gain access.</p>
-    <form method="post" action="WalkinRegistration.html">
+    <form method="post">
         <p>
             Username:
             <input type="text" name="username" />
@@ -27,39 +27,22 @@
             <input type="password" name="password" />
         </p>
         <br />
-        <input id="Submit" type="button" value="Submit" />
+        <input id="Submit" type="submit" value="Submit" />
     </form>
 
     <?php
-        $host="localhost3306";
-        $user="root";
-        $password="";
-        $db="paclregdb";
+        if(isset ($_POST['username']) && isset($_POST['password'])){
 
-    mysql_connect($host,$user, $password);
-    mysql_select_db($db);
+            $username = $_POST['username'];
+            $password=$_POST['password'];
 
-    if(isset($_POST['username']))
-{
-	$uname=$_POST['username'];
-	$password=$_POST['password']
+            $con = mysqli_connect("localhost", "brachaj_63", "zVgdb8mj");
+            
+            echo $username;
+            echo $password;
 
-	$sql = "select * from Admin where username='".$uname."'AND password='".$password."' limit 1";
+        }
 
-	$result=mysql_query($sql);
-
-	if(mysql_num_rows($result)==1)
-		{
-			echo "You have Sucessfully Logged in";
-			exit();
-		}
-
-	else
-		{
-		echo = "You have entered an incorrect password"
-		exit();
-		}
-}
     ?>
 
 
