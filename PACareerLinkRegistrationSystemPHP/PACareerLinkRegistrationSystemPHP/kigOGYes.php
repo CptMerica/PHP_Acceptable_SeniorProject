374cd12685989db0c6216166ba4b7a8178abc4a8
BRITTANI N KIGER <KIG6911@calu.edu>
2018-02-21 15:43:23 -0500

    I added a few pages for the registration system. Will update more this evening.

---
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/kigOGYes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Welcome to Registration</title>
+    <meta charset="utf-8" />
+
+    <link href="StyleSheet.css" rel="stylesheet" />
+    <img src="pacareerlink.jpg" style="height: 93px; width:459px;" />
+</head>
+<body>
+    <class:registration align="center">
+
+        <h2> Registration Form</h2>
+        <form method="post">
+            <class:registration>
+                <br />
+
+                <p>Would you like to update your information?</p>
+
+
+                <input type="button" onclick="window.location = 'kigUpdateInfo.php'" class="Redirect" value="Yes" />
+                <input type="button" onclick="window.location = 'kigNoUpdate.php'" class="Redirect" value="No" />
+
+        </form>
+    </class:registration>
+    <p align="bottomright">
+        Created by GJEF
+    </p>
+</body>
+</html>
