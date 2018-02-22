87b6b71c8949ed6e837dec76ede3263eb2a49dda
Stephen Terhorst <ter2198@calu.edu>
2018-02-22 18:50:00 -0500

    Established Connection

--- PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/DatabaseConnection.php
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/DatabaseConnection.php
@@ -1,49 +1,16 @@
 <?php
-//Connect To Database
-$hostname="localhost";
-$username="dynamicdeveloper";
-$password="burton23";
+$hostname = "160.153.77.232";
+$username="terhorst";
+$password="password";
 $dbname="paclregdb";
-$usertable="County";
-$yourfield = "COUNTY_DESC";
-$connection = mysqli_connect($hostname, $username, $password);
-mysqli_select_db($dbname, $connection);
-//Check connection
-if (mysqli_connect_error){
-    die("Connection Failed: " .mysqli_connect_error());
-}
-echo "connected successfully";
-$connection->close();
-?>
 
-<?php
+$con = mysqli_connect($hostname, $username, $password, $dbname);
 
-$servername = "localhost";
-$username = "dynamicdeveloper";
-$password = "burton23";
-$dbname = "paclregdb";
-$connection = mysqli_connect($hostname, $username, $password);
-mysqli_select_db($dbname, $connection);
-
-// Create connection
-$connection = new mysq($servername, $username, $password, $dbname);
-// Check connection
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
-
-$sql = "SELECT COUNTY_ID, COUNTY_DESC FROM COUNTY ";
-$result = $connection->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "County ID: " . $row["COUNTY_ID"]. " County Name: " . $row["COUTNY_DESC"]. "<br>";
-    }
+if(mysqli_connect_errno()){
+    echo mysqli_connect_error();
+    exit();
 } else {
-    echo "0 results";
+    echo "SUCESSFUL CONNECTION!";
 }
-$connection->close();
-
 
 ?>
