070614d9cbd54dbbd1e706b586972c9b0e295d42
Stephen Terhorst <ter2198@calu.edu>
2018-03-05 16:24:27 -0500

    Functional Report Page.

--- PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Reports.php
+++ PACareerLinkRegistrationSystemPHP/PACareerLinkRegistrationSystemPHP/Reports.php
@@ -1,12 +1,142 @@
+<?php
 
-<!DOCTYPE html>
+if(isset($_POST['fnamesearch']))
+{
+    $fnameToSearch = $_POST['firstnamesearch'];
+
+    $query = "SELECT * FROM WORKER WHERE F_NAME = '".$fnameToSearch."'";
+
+    $search_result = filterTable($query);
+
+}    else if(isset($_POST['lnamesearch']))
+{
+    $lnameToSearch = $_POST['lastnamesearch'];
+
+    $query = "SELECT * FROM WORKER WHERE L_NAME = '".$lnameToSearch."'";
+
+    $search_result = filterTable($query);
+    
+
+} else if(isset($_POST['l4ssnsearch']))
+    {
+        $l4ssnToSearch = $_POST['last4ssnsearch'];
+
+        $query = "SELECT * FROM WORKER WHERE LAST_4_SSN = '".$l4ssnToSearch."'";
+
+        $search_result = filterTable($query);
+
+
+
+}else if(isset($_POST['EDUsearch']))
+{
+    $educationToSearch = $_POST['EducationReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE EDUCATION = '".$educationToSearch."'";
+
+    $search_result = filterTable($query);
+
+
+} else if (isset($_POST['locationsearch'])){
+
+    $officeToSearch = $_POST['OfficeLocationReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE OFFICE_LOCATION = '".$officeToSearch."'";
+
+    $search_result = filterTable($query);
+
+
+} else if (isset($_POST['countysearch'])){
+
+    $countyToSearch = $_POST['CountyReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE COUNTY_RESIDENCE = '".$countyToSearch."'";
+
+    $search_result = filterTable($query);
+
+
+} else if (isset($_POST['empstatussearch'])){
+
+    $empstatusToSearch = $_POST['EmploymentReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE STATUS = '".$empstatusToSearch."'";
+
+    $search_result = filterTable($query);
+
+
+} else if (isset($_POST['55search'])){
+
+    $over55ToSearch = $_POST['Over55ReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE OVER_55= '".$over55ToSearch."'";
+
+    $search_result = filterTable($query);
+
+} else if (isset($_POST['youthsearch'])){
+
+    $youthToSearch = $_POST['YouthReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE YOUTH_STATUS = '".$youthToSearch."'";
+
+    $search_result = filterTable($query);
+} else if (isset($_POST['veteransearch'])){
+
+    $vetToSearch = $_POST['VetReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE VET_STATUS = '".$vetToSearch."'";
+
+    $search_result = filterTable($query);
+} else if (isset($_POST['fssearch'])){
+
+    $fsToSearch = $_POST['FoodStampsReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE FOOD_STAMPS = '".$fsToSearch."'";
+
+    $search_result = filterTable($query);
+
+} else if (isset($_POST['govasstsearch'])){
+
+    $govToSearch = $_POST['GovAssistanceReportSelect'];
+
+    $query = "SELECT * FROM WORKER WHERE GOVT_ASST = '".$govToSearch."'";
+
+    $search_result = filterTable($query);
+
+}
+
+    else {
+
+        $query = "SELECT * FROM `WORKER`";
+        $search_result = filterTable($query);
+
+        unset($_POST['OfficeLocationReportSelect']);
+        unset($_POST['EducationReportSelect']);
+    }
+
+
+
+function filterTable($query)
+{
+    $hostname = "www.scwdbpacl.com";
+    $username="terhorst";
+    $password="password";
+    $dbname="paclregdb";
+
+    $con = mysqli_connect($hostname, $username, $password, $dbname);
+
+
+    $filter_Result = mysqli_query($con, $query);
+
+    return $filter_Result;
+
+}
+
+
+?>
+
 <html>
 <head>
-    <title> Reports Page</title>
-    <meta charset="utf-8" />
-
-    <link href="StyleSheet.css" rel="stylesheet" />
-
+    <title>PACL Reports</title>
+    <link href="styleSheet.css" rel="stylesheet" />
 </head>
 <body>
 
@@ -19,31 +149,49 @@
             <li>
                 <a href="admin.php">Back</a>
             </li>
-           
-            <li>
-                <a style="float:right">Help</a>
+
+            <li style="float:right">
+                <a>Help</a>
             </li>
-          
+
         </ul>
     </div>
 
-
-    <class:reports align="center">
-
-        <h2> Report Form</h2>
-        <h2> Please fill out the from with the information you would like to build a report with. </h2>
-        <form method="post" id="reportform">
-            Location:
-            <select name="LocationReportSelect">
+    <form action="reports.php" method="post">
+        <p>Please select one category to search.</p>
+        <div>
+            First Name:
+            <input type="text" name="firstnamesearch"/>
+            <input type="submit" name="fnamesearch" value="Search"/>
+        </div>
+        <br />
+        <div>
+            Last Name:
+            <input type="text" name="lastnamesearch" />
+            <input type="submit" name="lnamesearch" value="Search" />
+        </div>
+        <br />
+        <div>
+            Last 4 SSN:
+            <input type="text" name="last4ssnsearch" />
+            <input type="submit" name="l4ssnsearch" value="Search" />
+        </div>
+        <br />
+        <div>
+            Site Location:
+            <select name="OfficeLocationReportSelect">
                 <option> </option>
                 <option> Beaver </option>
                 <option> Washington </option>
                 <option> Mon Valley </option>
                 <option> Greene </option>
             </select>
-            <br />
-            <br />
-            Workers Education:
+            <input type="submit" name="locationsearch" value="Search" />
+        </div>
+        <br />
+
+        <div>
+            Education:
             <select name="EducationReportSelect">
                 <option> </option>
                 <option> High School Diploma </option>
@@ -54,8 +202,11 @@
                 <option> GED </option>
                 <option> None </option>
             </select>
-            <br />
-            <br />
+            <input type="submit" name="EDUsearch" value="Search" />
+        </div>
+        <br />
+
+        <div>
             County of Residence:
             <select name="CountyReportSelect">
                 <option> </option>
@@ -68,8 +219,10 @@
                 <option>Butler</option>
                 <option> Greene </option>
             </select>
-            <br>
-            <br />
+            <input type="submit" name="countysearch" value="Search" />
+        </div>
+        <br>
+        <div>
             Employement Status:
             <select name="EmploymentReportSelect">
                 <option> </option>
@@ -77,86 +230,130 @@
                 <option> Employed </option>
                 <option> Dislocated</option>
             </select>
-            <br>
-            <br />
-            Select Person over 55?
+            <input type="submit" name="empstatussearch" value="Search"/>
+        </div>
+        <br />
+
+        <div>
+            People over 55
             <select name="Over55ReportSelect">
                 <option></option>
                 <option> Yes </option>
                 <option> No </option>
             </select>
-            Select Youth?
+            <input type="submit" name="55search" value="Search"/>
+        </div>
+        <br />
+
+        <div>
+            Between 14 and 24
             <select name="YouthReportSelect">
                 <option></option>
                 <option> Yes </option>
                 <option> No </option>
             </select>
-            Select Veteran?
+            <input type="submit" name="youthsearch" value="Search"/>
+        </div>
+        <br />
+        
+        <div>
+            Veteran
             <select name="VetReportSelect">
                 <option></option>
                 <option> Yes </option>
                 <option> No </option>
             </select>
-            Recieving Food Stamps?
+            <input type="submit" name="vetsearch" value="Search"/>
+        </div>
+        <br />
+        <div>
+            Recieving Food Stamps
             <select name="FoodStampsReportSelect">
                 <option></option>
                 <option> Yes </option>
                 <option> No </option>
             </select>
-            Recieving Government Assistance?
+            <input type="submit" name="fssearch" value="Search" />
+        </div>
+        <br />
+
+        <div>
+            Recieving Government Assistance
             <select name="GovAssistanceReportSelect">
                 <option> </option>
                 <option> Yes </option>
                 <option> No </option>
             </select>
-
-            <p align="center">
-                <input type="submit" onclick="window.location = 'ReportResults.php'" class="Redirect" value="Submit" name="submit" />
-            </p>
-        
-        </form>
+            <input type="submit" name="govasstsearch" value="search"/>
+        </div>
 
 
-    </class:reports>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Last 4 SSN:</th>
+                <th>Government Assistance</th>
+                <th>Food Stamps</th>
+                <th>Office Location</th>
+                <th>Residing County</th>
+                <th>Education</th>
+                <th>Employement Status</th>
+                <th>Purpose of Visit</th>
+                <th>Veteran Status</th>
+                <th>Over 55</th>
+                <th>Between 14 and 24</th>
+            </tr>
+
+
+            <?php while($row = mysqli_fetch_array($search_result)):?>
+            <tr>
+                <td>
+                    <?php echo $row['PARTICIPANT_ID'];?>
+                </td>
+                <td>
+                    <?php echo $row['F_NAME'];?>
+                </td>
+                <td>
+                    <?php echo $row['L_NAME'];?>
+                </td>
+                <td>
+                    <?php echo $row['LAST_4_SSN'];?>
+                </td>
+                <td>
+                    <?php echo $row['GOV_ASST'];?>
+                </td>
+                <td>
+                    <?php echo $row['FOOD_STAMPS'];?>
+                </td>
+                <td>
+                    <?php echo $row['OFFICE_LOCATION'];?>
+                </td>
+                <td>
+                    <?php echo $row['COUNTY_RESIDENCE'];?>
+                </td>
+                <td>
+                    <?php echo $row['EDUCATION'];?>
+                </td>
+                <td>
+                    <?php echo $row['STATUS'];?>
+                </td>
+                <td>
+                    <?php echo $row['VISIT_PURPOSE'];?>
+                </td>
+                <td>
+                    <?php echo $row['VET_STATUS'];?>
+                </td>
+                <td><?php echo $row['OVER_55'];?> </td>
+
+                <td><?php echo $row['YOUTH_STATUS'];?> </td>
+
+                
+            </tr>
+            <?php endwhile;?>
+        </table>
+    </form>
+
 </body>
 </html>
-
-<?php 
-    
-require_once('databaseConnection.php');
-
-
-    if (isset ($_POST['submit'])){
-        
-        $locationreport = $_POST['LocationReportSelect'];
-        $educationreport = $_POST['EducationReportSelect'];
-        $countyreport = $_POST['CountyReportSelect'];
-        $empstatusreport = $_POST['EmploymentReportSelect'];
-        $over55report = $_POST['Over55ReportSelect'];
-        $youthreport = $_POST['YouthReportSelect'];
-        $veteranreport = $_POST['VetReportSelect'];
-        $foodstampreport = $_POST['FoodStampsReportSelect'];
-        $govassistancereport = $_POST['GovAssistanceReportSelect'];
-
-        $sql = "SELECT * FROM WORKER WHERE COUNTY_RESIDENCE = '".$countyreport."'";
-
-        $result=$con ->query($sql);
-
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo"Participant_ID:".$row["PARTICIPANT_ID"].  "First Name:" .$row["F_NAME"]. "Last Name:" .$row["L_NAME"]. "Last Four SSN:" .$row["LAST_4_SSN"]. "Government Assistance:" .$row["GOV_ASST"]. "Food Stamps:" .$row["FOOD_STAMPS"]. "Office Location:" .$row["OFFICE_LOCATION"]. "Residing County:" . $row["COUNTY_RESIDENCE"]. "Education:" .$row["EDUCATION"]. "EMP STATUS:" .$row["STATUS"]. "Vist Reason:" .$row["VISIT_PURPOSE"]. "<br>";
-                
-                
-            }
-        } else {
-            echo "0 results";
-        }
-
-        
-    }
-
-
-
-?>
